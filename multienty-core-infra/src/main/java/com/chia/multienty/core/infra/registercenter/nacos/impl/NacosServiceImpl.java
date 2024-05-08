cdd7607a4f5947d17b156580e99b06d866c61d6f
kutasms <7437280@qq.com>
2024-05-09 06:57:14 +0800

    nacos service implementation.

--- multienty-core-infra/src/main/java/com/chia/multienty/core/infra/registercenter/nacos/impl/NacosServiceImpl.java
+++ multienty-core-infra/src/main/java/com/chia/multienty/core/infra/registercenter/nacos/impl/NacosServiceImpl.java
@@ -1,11 +1,11 @@
-package com.chia.multienty.core.fusion.registercenter.nacos.impl;
+package com.chia.multienty.core.infra.registercenter.nacos.impl;
 
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.ConfigType;
 import com.chia.multienty.core.properties.KutaNacosProperties;
-import com.chia.multienty.core.fusion.registercenter.nacos.NacosConfig;
+import com.chia.multienty.core.infra.registercenter.nacos.NacosConfig;
 import com.chia.multienty.core.domain.enums.SymbolEnum;
-import com.chia.multienty.core.fusion.registercenter.nacos.NacosService;
+import com.chia.multienty.core.infra.registercenter.nacos.NacosService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
@@ -49,6 +49,40 @@ public class NacosServiceImpl implements NacosService {
 
     @Override
     @SneakyThrows
+    public Map<String, Object> getObject(String dataId) {
+        String origRsp = nacosConfigService.getConfig(dataId, DEFAULT_GROUP, 5000);
+        Yaml yaml = new Yaml();
+        Map<String, Object> map = yaml.loadAs(origRsp, Map.class);
+        return map;
+    }
+
+    @Override
+    @SneakyThrows
+    public String getObjectStr(String dataId) {
+        return nacosConfigService.getConfig(dataId, DEFAULT_GROUP, 5000);
+    }
+
+    @Override
+    @SneakyThrows
+    public String getObjectStr(String dataId, String group) {
+        return nacosConfigService.getConfig(dataId, group, 5000);
+    }
+
+    @Override
+    @SneakyThrows
+    public String getString(String dataId) {
+        return nacosConfigService.getConfig(dataId, DEFAULT_GROUP, 5000);
+    }
+
+    @Override
+    @SneakyThrows
+    public String getString(String dataId, String group) {
+        return nacosConfigService.getConfig(dataId, group, 5000);
+    }
+
+
+    @Override
+    @SneakyThrows
     public Map<String, Object> getObject(String dataId, String group) {
         String origRsp = nacosConfigService.getConfig(dataId, group, 5000);
         Yaml yaml = new Yaml();
@@ -72,5 +106,26 @@ public class NacosServiceImpl implements NacosService {
         String dump = yaml.dumpAs(data, Tag.MAP, DumperOptions.FlowStyle.BLOCK);
 
         nacosConfigService.publishConfig(dataId, group, dump, ConfigType.YAML.getType());
+    }
+
+    @Override
+    @SneakyThrows
+    public void updateYamlConfig(String data, String dataId, String group) {
+        nacosConfigService.publishConfig(dataId, group, data, ConfigType.YAML.getType());
+    }
+
+    @Override
+    @SneakyThrows
+    public void updateYamlConfig(String data, String dataId) {
+        nacosConfigService.publishConfig(dataId, DEFAULT_GROUP, data, ConfigType.YAML.getType());
+    }
+
+    @SneakyThrows
+    @Override
+    public void updateYamlConfig(Object data, String dataId) {
+        Yaml yaml = new Yaml();
+        String dump = yaml.dumpAs(data, Tag.MAP, DumperOptions.FlowStyle.BLOCK);
+
+        nacosConfigService.publishConfig(dataId, DEFAULT_GROUP, dump, ConfigType.YAML.getType());
     }
 }
